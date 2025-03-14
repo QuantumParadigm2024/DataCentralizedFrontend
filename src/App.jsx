af07a203687a388554fa089e90aab3b8f4fe22c9
QuantumParadigm2024 <quantumshare12@gmail.com>
2025-03-14 11:12:58 +0530

    Code v0.8

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import Dashboard from './Components/Dashboard'
 import Recent from './Components/Recent'
 import Bin from './Components/Bin'
 import Collections from './Components/Collections'
+import './global.css'
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Dashboard />} />
-                <Route path='recent' element={<Recent/>}/>
-                <Route path='bin' element={<Bin/>} />
-                <Route path='collections' element={<Collections/>}/>
+                <Route path='recent' element={<Recent />} />
+                <Route path='bin' element={<Bin />} />
+                <Route path='collections' element={<Collections />} />
             </Routes>
         </BrowserRouter>
     )
